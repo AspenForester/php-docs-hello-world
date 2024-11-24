e024aa2ab9fd1bf595e9a811043f1d3ac9bdd84d
AspenForester <jblewis@gmail.com>
2024-11-23 21:49:19 -0600

    OK, maybe not a button script

--- index.php
+++ index.php
@@ -6,12 +6,12 @@ echo '<script type="text/JavaScript">
      alert("I am a JavaScript Alert"); 
      </script>' 
 ; 
-*/
+
 echo '<script type="text/JavaScript"> 
     <button type="button"
     onclick="document.getElementById('demo').innerHTML = Date()">
     Click me to display Date and Time.</button>
     </script>'
-
+*/
 //phpinfo();
 ?>
