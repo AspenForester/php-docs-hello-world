d3c80d3ca7a82e536d5f2472e1e5393566c2cf11
AspenForester <jblewis@gmail.com>
2024-11-23 21:44:57 -0600

    Let's try returning the date time with JavaScript

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
 
 echo "Hello Azure!";
-
+/*
 echo '<script type="text/JavaScript">  
      alert("I am a JavaScript Alert"); 
      </script>' 
 ; 
+*/
+echo '<script type="text/JavaScript"> 
+    <button type="button"
+    onclick="document.getElementById('demo').innerHTML = Date()">
+    Click me to display Date and Time.</button>
+    </script>'
 
-phpinfo();
+//phpinfo();
 ?>
